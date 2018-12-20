b3b41a662b803032967e34a0ba08c8c8fdb59b62
Martin Weihrauch <martin.weihrauch@web.de>
2018-12-20 20:46:02 +0100

    RStudio commit

--- MovieLens_project.R
+++ MovieLens_project.R
@@ -55,8 +55,14 @@ write.csv(validation %>% select(userId, movieId) %>% mutate(rating = NA),
 rm(dl, ratings, movies, test_index, temp, movielens, removed)
 
 
-
 # Begin
+
+# We generate training and testing sets
+set.seed(1)
+idx <- createDataPartition(edx$rating, times = 1, p = 0.5, list = FALSE)
+train_set <- edx[idx, ]
+test_set <- edx[-idx, ]
+
 # We write a function that computes the Residual Mean Squared Error ("typical error")
 RMSE <- function(true_ratings, predicted_ratings){
   sqrt(mean((true_ratings - predicted_ratings)^2))
@@ -64,7 +70,8 @@ RMSE <- function(true_ratings, predicted_ratings){
 
 
 # Simplest model: We predict a new rating to be the average rating of all movies, which gives us a baseline error value
-naive_RMSE <- RMSE(edx$rating, mu)
+mu <- mean(train_set$rating)
+naive_RMSE <- RMSE(test_set$rating, mu)
 naive_RMSE
 
 # We generate a table to record our approaches and the error they generate
@@ -72,17 +79,17 @@ rmse_results <- data_frame(method = "Just the average rating", RMSE = naive_RMSE
 rmse_results
 
 # Are different movies rated differently? We compute the average rating b (as in "bias") for a movie i: b_i
-mu <- mean(edx$rating)
-movie_avgs <- edx %>%
+
+movie_avgs <- train_set %>%
   group_by(movieId) %>%
   summarize(b_i = mean(rating - mu))
 
 # We predict movie ratings based on the fact that different movies are rated differently
-predicted_ratings <- mu + edx %>% 
+predicted_ratings <- mu + train_set %>% 
   left_join(movie_avgs, by = 'movieId') %>%
   .$b_i
 
-model_1_RMSE <- RMSE(predicted_ratings, edx$rating)
+model_1_RMSE <- RMSE(predicted_ratings, train_set$rating)
 rmse_results <- bind_rows(rmse_results, data_frame(method = "Movie Effect Model", RMSE = model_1_RMSE))
 rmse_results
 
@@ -90,18 +97,18 @@ rmse_results
 
 # Do different users rate different movies differently? We compute b_u, the user-specific effect
 
-user_avgs <- edx %>%
+user_avgs <- train_set %>%
   left_join(movie_avgs, by = 'movieId') %>%
   group_by(userId) %>%
   summarize(b_u = mean(rating - mu - b_i))
 
-predicted_ratings2 <- edx %>%
+predicted_ratings2 <- train_set %>%
   left_join(movie_avgs, by = 'movieId') %>%
   left_join(user_avgs, by = 'userId') %>%
   mutate(pred = mu + b_i + b_u) %>%
   .$pred
 
-model_2_RMSE <- RMSE(predicted_ratings2, edx$rating)
+model_2_RMSE <- RMSE(predicted_ratings2, train_set$rating)
 rmse_results <- bind_rows(rmse_results, data_frame(method = "Movie + User Effects Model", RMSE = model_2_RMSE))
 rmse_results
 
