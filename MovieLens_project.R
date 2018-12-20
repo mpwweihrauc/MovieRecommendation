792fd78701ebac0532968c783eab51553b58c277
Martin Weihrauch <martin.weihrauch@web.de>
2018-12-20 18:29:18 +0100

    RStudio commit

--- MovieLens_project.R
+++ MovieLens_project.R
@@ -56,4 +56,62 @@ rm(dl, ratings, movies, test_index, temp, movielens, removed)
 
 
 
-# It begins, finally
+# Begin
+# We write a function that computes the Residual Mean Squared Error ("typical error")
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))
+}
+
+
+# Simplest model: We predict a new rating to be the average rating of all movies, which gives us a baseline error value
+naive_RMSE <- RMSE(edx$rating, mu)
+naive_RMSE
+
+# We generate a table to record our approaches and the error they generate
+rmse_results <- data_frame(method = "Just the average rating", RMSE = naive_RMSE)
+rmse_results
+
+# Are different movies rated differently? We compute the average rating b (as in "bias") for a movie i: b_i
+mu <- mean(edx$rating)
+movie_avgs <- edx %>%
+  group_by(movieId) %>%
+  summarize(b_i = mean(rating - mu))
+
+# We predict movie ratings based on the fact that different movies are rated differently
+predicted_ratings <- mu + edx %>% 
+  left_join(movie_avgs, by = 'movieId') %>%
+  .$b_i
+
+model_1_RMSE <- RMSE(predicted_ratings, edx$rating)
+rmse_results <- bind_rows(rmse_results, data_frame(method = "Movie Effect Model", RMSE = model_1_RMSE))
+rmse_results
+
+# We can see that the RMSE improved when we take into account that different movies are rated differently
+
+# Do different users rate different movies differently? We compute b_u, the user-specific effect
+
+user_avgs <- edx %>%
+  left_join(movie_avgs, by = 'movieId') %>%
+  group_by(userId) %>%
+  summarize(b_u = mean(rating - mu - b_i))
+
+predicted_ratings2 <- edx %>%
+  left_join(movie_avgs, by = 'movieId') %>%
+  left_join(user_avgs, by = 'userId') %>%
+  mutate(pred = mu + b_i + b_u) %>%
+  .$pred
+
+model_2_RMSE <- RMSE(predicted_ratings2, edx$rating)
+rmse_results <- bind_rows(rmse_results, data_frame(method = "Movie + User Effects Model", RMSE = model_2_RMSE))
+rmse_results
+
+# We can see that by taking into account both the movie and the user effect, we can further improve our RMSE
+
+
+# We can use Regularization to improve our predictions
+
+
+
+# Adding a new column "date" with proper datetime
+library(lubridate)
+edx <- mutate(edx, date = as_datetime(timestamp))
