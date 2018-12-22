5e6200f60a4aaf7ebda99708c826749c6e587ed8
Martin Weihrauch <martin.weihrauch@web.de>
2018-12-22 16:34:51 +0100

    RStudio commit

--- MovieLens_project.R
+++ MovieLens_project.R
@@ -56,32 +56,56 @@ rm(dl, ratings, movies, test_index, temp, movielens, removed)
 
 
 # Begin
+# Exploratory data analysis, we are getting familiar with our dataset
+
+# Each row represents a rating given by a user to a movie
+head(edx) 
+
+# We are dealing with ~70000 unique users giving ratings to ~ 10700 different movies
+edx %>% 
+  summarize(n_users = n_distinct(userId), n_movies = n_distinct(movieId))
+
+# Some movies are rated more often than others
+edx %>% 
+  count(movieId) %>% 
+  ggplot(aes(n)) + 
+  geom_histogram(bins = 30, color = "black") + 
+  scale_x_log10() + 
+  ggtitle("Movies")
 
 
-# We write a function that computes the Residual Mean Squared Error ("typical error")
+
+
+# We write a loss-function that computes the Residual Mean Squared Error ("typical error") as
+# our measure of accuracy. The value is the typical error in star rating we would make
 RMSE <- function(true_ratings, predicted_ratings){
   sqrt(mean((true_ratings - predicted_ratings)^2))
 }
 
-
-# Simplest model: We predict a new rating to be the average rating of all movies, which gives us a baseline error value
+# Simplest model: We predict a new rating to be the average rating of all movies in our dataset,
+# which gives us a baseline RMSE. We observe that the mean movie rating is a pretty generous > 3.5.
 mu <- mean(edx$rating)
-naive_RMSE <- RMSE(edx$rating, mu)
-naive_RMSE
+baseline_RMSE <- RMSE(edx$rating, mu)
+baseline_RMSE
 
-# We generate a table to record our approaches and the error they generate
-rmse_results <- data_frame(method = "Just the average rating", RMSE = naive_RMSE)
+# We generate a table to record our approaches and the RMSEs they generate.
+rmse_results <- data_frame(method = "Simplest model: Average rating", RMSE = baseline_RMSE)
 rmse_results
 
-# Are different movies rated differently? We compute the average rating b (as in "bias") for a movie i: b_i
+# Are different movies rated differently? We compute the average rating "b" (as in "bias") for a movie "i": b_i
 
 movie_avgs <- edx %>%
   group_by(movieId) %>%
   summarize(b_i = mean(rating - mu))
 
-movie_avgs %>% qplot(b_i, geom ="histogram", bins = 10, data = ., color = I("black"))
+# By plotting our computed b_i, we can see that indeed, there are large differences in average movie ratings
+movie_avgs %>% qplot(b_i, geom = "histogram", bins = 10, data = ., color = I("black"))
 
-# We predict movie ratings based on the fact that different movies are rated differently
+# We predict movie ratings based on the fact that different movies are rated differently by adding mu and our computed b_is
+# If an individual movie is on average rated worse than the average rating of all movies "mu",
+# we predict that it will be rated lower than "mu" by "b_i", the differecnce of the individual movie
+# average from the total average
+
 predicted_ratings <- mu + edx %>% 
   left_join(movie_avgs, by = 'movieId') %>%
   .$b_i
@@ -90,23 +114,29 @@ model_1_RMSE <- RMSE(edx$rating, predicted_ratings)
 rmse_results <- bind_rows(rmse_results, data_frame(method = "Movie Effect Model", RMSE = model_1_RMSE))
 rmse_results
 
-# We can see that the RMSE improved when we take into account that different movies are rated differently
+# We can see that the RMSE improved when we take into account that different movies are rated differently.
 
-# Do different users rate different movies differently? We compute b_u, the user-specific effect
-# We can see that a few uers rate movies generally higher/lower than others, while most fall in-between
+# Do users rate different movies differently? We compute b_u, the user-specific effect.
+# We can see that some users rate movies generally higher/lower than others,
+# while most fall in-between, but also that user rating of movies is generally higher
+# than a 2.5 "true average" rating. This was reflected previously in the high mean rating of > 3.5.
+
+# Visualization of mean rating by users with over 100 rated movies.
+# Some users are very critical, some are rather generous.
 edx %>% 
   group_by(userId) %>% 
-  summarize(b_u = mean(rating)) %>% 
   filter(n()>=100) %>%
+  summarize(b_u = mean(rating)) %>% 
   ggplot(aes(b_u)) + 
   geom_histogram(bins = 30, color = "black")
 
-
+# Calculation of the user effect b_u, taking into account b_i
 user_avgs <- edx %>%
   left_join(movie_avgs, by = 'movieId') %>%
   group_by(userId) %>%
   summarize(b_u = mean(rating - mu - b_i))
 
+# We predict ratings taking into account b_i and b_u
 predicted_ratings <- edx %>%
   left_join(movie_avgs, by = 'movieId') %>%
   left_join(user_avgs, by = 'userId') %>%
@@ -142,20 +172,3 @@ movie_titles <- edx %>%
 colnames(y) <- with(movie_titles, title[match(colnames(y), movieId)])
 y <- sweep(y, 2, colMeans(y, na.rm=TRUE))
 y <- sweep(y, 1, rowMeans(y, na.rm=TRUE))
-
-
-
-
-# Rounding my predictions
-my_reference <- factor(edx$rating)
-my_prediction <- predicted_ratings
-my_prediction[my_prediction < 0.5] <- 0.5
-my_prediction[my_prediction > 5] <- 5
-
-my_prediction <- round(my_prediction, digits = 1)
-my_prediction <- round(my_prediction*2)/2
-my_prediction <- factor(my_prediction)
-
-confusionMatrix(my_prediction, my_reference)
-
-
